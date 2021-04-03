bee7a4e840fb665115ec46f60222fb8898e0d4c8
JigglyNoahPuff <38081350+JigglyNoahPuff@users.noreply.github.com>
2021-04-03 02:26:52 -0600

    Noah: Pig Latin Cipher

--- lab13_/cipher04.h
+++ lab13_/cipher04.h
@@ -1,4 +1,3 @@
-#pragma once
 /********************************************************************
 * Header:
 *    Implement your cipher here. You can view 'example.h' to see the
@@ -15,10 +14,10 @@
 class Cipher04 : public Cipher
 {
 public:
-    virtual std::string getPseudoAuth() { return "pseudocode author"; }
-    virtual std::string getCipherName() { return "cipher name"; }
-    virtual std::string getEncryptAuth() { return "encrypt author"; }
-    virtual std::string getDecryptAuth() { return "decrypt author"; }
+    virtual std::string getPseudoAuth() { return "Noah Cook"; }
+    virtual std::string getCipherName() { return "Noah's pig latin"; }
+    virtual std::string getEncryptAuth() { return "Noah Cook"; }
+    virtual std::string getDecryptAuth() { return "Noah Cook"; }
 
     /***********************************************************
      * GET CIPHER CITATION
@@ -26,7 +25,7 @@ public:
      ***********************************************************/
     virtual std::string getCipherCitation()
     {
-        return std::string("citation");
+        return std::string("https://www.wattpad.com/432553860-codes-and-ciphers-pig-latin");
     }
 
     /**********************************************************
@@ -39,36 +38,49 @@ public:
 
         // TODO: please format your pseudocode
         // The encrypt pseudocode
-        str = "insert the encryption pseudocode\n";
+        str = "If word starts with consonant and last letter put consonant to the end and add ay?\nelse add way! to the end\n";
 
         // The decrypt pseudocode
-        str += "insert the decryption pseudocode\n";
+        str += "if encryption ends in ? remove last three and move the fourth from the end to the front\nelse remove last 4\n";
 
         return str;
     }
 
     /**********************************************************
      * ENCRYPT
-     * TODO: ADD description
+     * TODO: Encrypts the plainText into Noah's version of 
+     * piglatin. password is ignored
      **********************************************************/
     virtual std::string encrypt(const std::string& plainText,
         const std::string& password)
     {
         std::string cipherText = plainText;
-        // TODO - Add your code here
+        char c = cipherText[0];
+        
+        if (c == 'A' || c == 'E' || c == 'I' || 
+            c == 'O' || c == 'U' || c == 'a' || 
+            c == 'e' || c == 'i' || c == 'o' ||
+            c == 'u') {
+            cipherText = cipherText + "way!";
+            return cipherText;
+            }
+        cipherText = cipherText.substr(1, cipherText.size()) + cipherText[0] + "ay?";
         return cipherText;
     }
 
     /**********************************************************
      * DECRYPT
-     * TODO: ADD description
+     * TODO: Decrypts from Noah's piglatin into plainText.
+     * password is still ignored.
      **********************************************************/
     virtual std::string decrypt(const std::string& cipherText,
         const std::string& password)
     {
         std::string plainText = cipherText;
-        // TODO - Add your code here
-        return plainText;
+        if (plainText[plainText.size() - 1] == '!') {
+            return plainText.substr(0, plainText.size() - 4);
+        }
+        return plainText[plainText.size() - 4] + plainText.substr(0, plainText.size() - 4);
     }
 };
 
